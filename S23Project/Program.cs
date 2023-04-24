9872c1b33d2c826c0d77b9f7c38f1afab9d4ff68
greta <greta@GRETA-COM>
2023-04-24 16:00:57 -0500

    Add project files.

---
+++ S23Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace S23Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
